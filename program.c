b9dbbb417ec93fbeae05740fcf80324f1f78f6af
vitorindeep <v.j.r.c1997@hotmail.com>
2017-03-24 13:10:47 +0000

    Program.c beginning
    Should be ready to rock some tests ;)

--- program.c
+++ program.c
@@ -1,1 +1,24 @@
 #include "interface.h"
+
+int main(int argc, char const *argv[]) {
+
+TAD_istruct estrutura;
+
+estrutura = init();
+load(estrutura, int nsnaps, char* snaps_paths[]);
+
+all_articles(TAD_istruct estrutura);
+unique_articles(TAD_istruct estrutura);
+all_revisions(TAD_istruct estrutura);
+top_10_contributors(TAD_istruct estrutura);
+contributor_name(long contributor_id, TAD_istruct estrutura);
+top_20_largest_articles(TAD_istruct estrutura);
+article_title(long article_id, TAD_istruct estrutura);
+top_N_articles_with_more_words(int n, TAD_istruct estrutura);
+titles_with_prefix(char* prefix, TAD_istruct estrutura);
+article_timestamp(long article_id, long revision_id, TAD_istruct estrutura);
+
+clean(estrutura);
+
+	return 1;
+}
