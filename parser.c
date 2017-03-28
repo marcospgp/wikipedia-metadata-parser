cc55bde08053569c50b5b3b113d6ac38ee4686c2
Marcos Pereira <marcospereiradev@gmail.com>
2017-03-28 13:29:05 +0100

    Adicionar função parsePage

--- parser.c
+++ parser.c
@@ -5,6 +5,28 @@
 #include <libxml/parser.h>
 #include "parser.h"
 
+void parsePage(xmlDocPtr doc, xmlNodePtr cur) {
+
+	printf("Parsing a page! Cur: %s\n",  cur->name);
+
+	cur = cur->children;
+
+	while(cur != NULL) {
+
+		if (cur->type == XML_ELEMENT_NODE) {
+
+			printf("%s\n", cur->name);
+
+			// Elemento "revision"
+			if (!xmlStrcmp(cur->name, (const xmlChar *) "revision")) {
+				parsePage(doc, cur);
+			}
+		}
+
+		cur = cur->next;
+	}
+}
+
 void parse(const char *docname) {
 
 	xmlDocPtr doc;
@@ -31,14 +53,15 @@ void parse(const char *docname) {
 		return;
 	}
 
-	// Loop principal
+	// Loop principal - itera pelas páginas
 
 	cur = cur->children;
 
-	while (cur != null) {
+	while (cur != NULL) {
 
-		if ((!xmlStrcmp(cur->name, (const xmlChar *)"storyinfo"))){
-			parseStory (doc, cur);
+		if ((!xmlStrcmp(cur->name, (const xmlChar *)"page"))){
+
+			parsePage(doc, cur);
 		}
 
 		cur = cur->next;
@@ -47,41 +70,3 @@ void parse(const char *docname) {
 	// Libertar a memória
 	xmlFreeDoc(doc);
 }
-
-/*
-//o array que vai receber os dados
-// o array tem um tamanho limitado, vai ser imprevisivel o comportamento após 50 caracteres
-static char array[20][50];
-
-void parse(const char *nomedoc) {
-
-	xmlDocPtr data = xmlParseFile(nomedoc);
-	xmlNodePtr cur = xmlDocGetRootElement(data);
-	xmlChar *conteudo;
-
-	if (data == NULL) {
-		printf("O parse nao foi feito \n");
-		return;
-	}
-
-	if (cur == NULL) {
-		printf("documento vazio \n");
-		xmlFreeDoc(data);
-		return;
-	}
-
-	int i = 0;
-
-	for (cur = cur->children; cur != NULL; cur = cur->next) {
-		if (cur->type != XML_ELEMENT_NODE) continue;
-		conteudo = xmlNodeGetContent(cur);
-		strcpy(array[i], conteudo);
-		printf("Conteudo no array na posição %d de <page>: %s\n", i, array[i]);
-		i++;
-		// printf("Conteudo lido de <page>: %s\n", conteudo);
-		xmlFree(conteudo);
-	}
-
-	xmlFreeDoc(data);
-}
-*/
