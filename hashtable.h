8f8196d110ee74385fd407eacf57ea849abad046
Marcos Pereira <marcospereiradev@gmail.com>
2017-03-30 18:09:28 +0100

    Adicionar dados ao header hashtable

--- hashtable.h
+++ hashtable.h
@@ -1,20 +1,37 @@
 #ifndef hashtable_h
 #define hashtable_h
 
+typedef struct __article {
+	long id, size, words;
+	char title[255]; // Limite de tamanho de título da wikipédia
+} article;
+
 typedef struct __user {
 	long id, contributions;
 	char username[255];
 } user;
 
 typedef struct __article {
-	long id, size, words;
-	char title[255];
-} article;
+	long id;
+	char timestamp[255];
+} revision;
 
 void init();
 
-void insertOrUpdateUser(long id, char *username, long contributions);
+/*
+	Esta função deve ser chamada quando é encontrada uma contribuição de um certo utilizador.
+	O módulo deve manter internamente o cálculo do número de contribuições de cada utilizador.
+*/
+void onUserContribution(long id, char *username);
 
-void insertOrUpdateArticle(long id, char *username, long contributions);
+/*
+	Esta função deve ser chamada quando é encontrada uma revisão de um artigo.
+	A função cria ou atualiza um artigo já existence na tabela. Caso seja feita uma
+	atualização, o tamanho do artigo e número de palavras só é atualizado se for
+	maior que o anterior. Os valores restantes são sempre atualizados.
+*/
+void onRevision(long id, char *title, long revisionId, char *revisionTimestamp, long sizeBytes, long nWords);
+
+// TODO - função que permita fazer loop pelas hash tables
 
 #endif
