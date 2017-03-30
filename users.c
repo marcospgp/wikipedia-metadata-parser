04602b7169216a0daa35863f5a92cc171d093930
vitorindeep <v.j.r.c1997@hotmail.com>
2017-03-30 22:21:55 +0100

    Added revisionTimestamp on parser AND doing the articles work

--- users.c
+++ users.c
@@ -4,6 +4,12 @@
 
 #include "users.h"
 
+/*
+	-> no módulo, vai ter de chamar a função (da HASH) de procura por ID
+		-> se encontrar, chama a função (da HASH) que aumenta o numOfContributions
+		-> se não encontrar, chama a função (da HASH) que insere o ID+username+counter=1
+*/
+
 
 TAD_istruct onPageUsers(TAD_istruct qs, long revisionContributorId, char *revisionContributorUsername) {
 
