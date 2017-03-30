6a1b4f6806e7fcdd4ac2f55936336e4fc15b5649
vitorindeep <v.j.r.c1997@hotmail.com>
2017-03-30 22:56:38 +0100

    Articles - FALTA TESTAR
    E DEPOIS REZAR AOS DEUSES DOS SERVIDORES DO DI PARA FUNCIONAREM.

--- articles.c
+++ articles.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hashtable.h"
 
 #include "articles.h"
+
+// TENHO DE TESTAR!!
+
+long wordCounter(char *revisionText) {
+	char *str;
+	int i;
+	long count = 0;
+	int foundLetter = False;
+
+	for (i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ')
+			foundLetter = False;
+		else {
+      		if (foundLetter == False)
+        		count++;
+    	foundLetter = True;
+		}
+	}
+
+	return count;
+}
 
 /*
 	-> chama a função de procura (da HASH) ID do artigo
@@ -24,7 +46,17 @@ TAD_istruct onPageArticles(
     char *revisionTimestamp
 ) {
 
+// TODO - fazer a função que conta o número de palavras e a função que conta o número de caracteres
 
-	qs = onRevision(articleId, title, revisionId, revisionTimestamp, long sizeBytes, long nWords);
+	long sizeBytes = strlen(revisionText)
+	long nWords = wordCounter(revisionText);
+
+
+/* Esta função deve ser chamada quando é encontrada uma revisão de um artigo.
+ * A função cria ou atualiza um artigo já existence na tabela. Caso seja feita uma
+ * atualização, o tamanho do artigo e número de palavras só é atualizado se for
+ * maior que o anterior. Os valores restantes são sempre atualizados.
+*/
+	qs = onRevision(articleId, title, revisionId, revisionTimestamp, sizeBytes, nWords);
 
 }
