14b7691fbd583a3381146302055ac3873c0061c0
sergiotj97 <stoj97@gmail.com>
2017-04-02 18:24:31 +0100

    Adicionar users a hashtable

--- hashtable.c
+++ hashtable.c
@@ -19,6 +19,27 @@ TAD_istruct initHashtables(TAD_istruct qs) {
 
 TAD_istruct onUserContribution(TAD_istruct qs, long id, char *username) {
 
+	// Procurar pelo utilizador na hash table
+	struct user *userData = (struct user*) g_hash_table_lookup(qs->users, &id);
+
+	if (userData == NULL) {
+
+		// Criar novo utilizador
+
+		char *usernameCopy = g_strdup(username); // Copiar string porque o parser pode apagá-la
+
+		struct user newUser = {id, 1, usernameCopy};
+
+		userData = &newUser;
+
+	} else {
+		userData->contributions = (userData->contributions) + 1;
+	}
+
+	// Inserir utilizador na hash table
+	g_hash_table_insert(qs->users, &id, &userData);
+
+	return qs;
 }
 
 TAD_istruct onRevision(TAD_istruct qs, long id, char *title, long revisionId, char *revisionTimestamp, long sizeBytes, long nWords) {
