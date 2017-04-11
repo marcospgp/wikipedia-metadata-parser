f7d08cbee987808b0e7b643076e8cf30d91e28d5
sergiotj97 <stoj97@gmail.com>
2017-04-11 20:00:22 +0100

    inseridos testes em comentario

--- program.c
+++ program.c
@@ -19,29 +19,85 @@
 /*
 int corretos = 0;
 
+teste para long all_articles(TAD_istruct qs);
 printf("Testing all_articles...\n");
 long allArticlesResult = all_articles(qs);
 if (allArticlesResult == NUM_ALL_ARTICLES) {printf("CORRETO!\n"); corretos++;}
 else printf("Valor esperado: %ld\n Valor obtido: %ld", NUM_ALL_ARTICLES, allArticlesResult);
 
+teste para long unique_articles(TAD_istruct qs);
 printf("Testing unique_articles...\n");
 long uniqueArticlesResult = unique_articles(qs);
 if (uniqueArticlesResult == NUM_UNIQUE_ARTICLES) {printf("CORRETO!\n"); corretos++;}
 else printf("Valor esperado: %d\n Valor obtido: %d", NUM_UNIQUE_ARTICLES, uniqueArticlesResult);
 
+teste para long all_revisions(TAD_istruct qs);
 printf("Testing all_revisions...\n");
 long allRevisionsResult = all_revisions(qs);
 if (allRevisionsResult == NUM_ALL_REVISIONS) {printf("CORRETO!\n"); corretos++;}
 else printf("Valor esperado: %d\n Valor obtido: %d", NUM_ALL_REVISIONS, allRevisionsResult);
 
+teste/print dos long* top_10_contributors(TAD_istruct qs);
+for(int i = 0;contributor_rank < 11;i++){
+	printf("contributor com rank %d: %s \n",i,contributor_name(i));
+}
 
-long* top_10_contributors(TAD_istruct qs);
-char* contributor_name(long contributor_id, TAD_istruct qs);
-long* top_20_largest_articles(TAD_istruct qs);
-char* article_title(long article_id, TAD_istruct qs);
-long* top_N_articles_with_more_words(int n, TAD_istruct qs);
+teste para char* contributor_name(long contributor_id, TAD_istruct qs);
+printf("Testing contributor_name...\n");
+printf("Is user 9092818 Paine Ellsworth?\n");
+query = contributor_name((long) 9092818, qs);
+printf("Answer: %s\n\n", query);
+printf("Is user 17585538 Telfordbuck?\n");
+query = contributor_name((long) 17585538, qs);
+printf("Answer: %s\n\n", query);
+printf("Is user 7328338 Yobot?\n");
+query = contributor_name((long) 7328338, qs);
+printf("Answer: %s\n\n", query);
+printf("Is user 9635580 Vitor?\n");
+query = contributor_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+printf("Is user 9635580 Marcos?\n");
+query = contributor_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+printf("Is user 9635580 Sergio?\n");
+query = contributor_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+
+teste/print dos long* top_20_largest_articles(TAD_istruct qs);
+for(int i = 0;article_rank < 21;i++){
+	printf("article com rank %d: %s \n",i,article_name(i));
+}
+
+teste para char* article_title(long article_id, TAD_istruct qs);
+printf("Testing article_title...\n");
+printf("Is article 9092818 Paine Ellsworth?\n");
+query = article_name((long) 9092818, qs);
+printf("Answer: %s\n\n", query);
+printf("Is article 17585538 Telfordbuck?\n");
+query = article_name((long) 17585538, qs);
+printf("Answer: %s\n\n", query);
+printf("Is article 7328338 Yobot?\n");
+query = article_name((long) 7328338, qs);
+printf("Answer: %s\n\n", query);
+printf("Is article 9635580 Vitor?\n");
+query = article_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+printf("Is article 9635580 Marcos?\n");
+query = article_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+printf("Is article 9635580 Sergio?\n");
+query = article_name((long) 9635580, qs);
+printf("Answer: %s\n\n", query);
+
+teste/print para long* top_N_articles_with_more_words(int n, TAD_istruct qs);
+for(int i = 0;article_top < n+1;i++){
+	printf("article com rank %d: %s \n",i,article(i));
+}
+
 char** titles_with_prefix(char* prefix, TAD_istruct qs);
+
 char* article_timestamp(long article_id, long revision_id, TAD_istruct qs);
+
 //TAD_istruct clean(TAD_istruct qs);
 
 printf("Acertaste em %d de 10 testes.\n", corretos);
