1c23e46157447231630deb6ddc92b5d830d28f7b
vitorindeep <v.j.r.c1997@hotmail.com>
2017-06-08 17:29:58 +0100

    Debugging Parser and adding teste xml

--- java_project/src/main/java/engine/Parser.java
+++ java_project/src/main/java/engine/Parser.java
@@ -2,15 +2,18 @@ package engine;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 
-import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
@@ -33,7 +36,6 @@ public class Parser {
     for(Article curArticle : articles.values()){
       System.out.println(curArticle.toString()); // tenho de definir toString do articles
     }
-    */
 
     // Testar recolha de dados de users
     /*
@@ -51,78 +53,106 @@ public class Parser {
     Article curArticle = null;
     User curUser = null;
     Revision curRevision = null;
+    boolean hasPageTitle = false, hasPageId = false,
+            hasRevisionId = false, hasRevisionParentId = false, hasRevisionTimestamp = false,
+            hasContributorUsername = false, hasContributorId = false,
+            onContributor = false, onRevision = false;
 
 
-    XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+    System.out.println("Comecei o parse.\n");
+
     try {
-      XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(fileName));
-      while(xmlEventReader.hasNext()){
-        XMLEvent xmlEvent = xmlEventReader.nextEvent();
-        if (xmlEvent.isStartElement()){
-          StartElement startElement = xmlEvent.asStartElement();
-          // COMEÇA NOVA PAGE
-          if(startElement.getName().getLocalPart().equals("page")) {
-            curArticle = new Article();
-            curUser = new User();
-          }
-          else if(startElement.getName().getLocalPart().equals("title")) {
-            xmlEvent = xmlEventReader.nextEvent();
-            curArticle.setArticleTitle(xmlEvent.asCharacters().getData());
-          }
-          else if(startElement.getName().getLocalPart().equals("id")) {
-            xmlEvent = xmlEventReader.nextEvent();
-            curArticle.setArticleId(Long.parseLong(xmlEvent.asCharacters().getData()));
-            curUser.setUserId(Long.parseLong(xmlEvent.asCharacters().getData()));
-          }
-          // PERCORRE A REVISION
-          else if(startElement.getName().getLocalPart().equals("revision")){
-            xmlEvent = xmlEventReader.nextEvent();
-            curRevision = new Revision();
-            while(xmlEventReader.hasNext()) {
-              xmlEvent = xmlEventReader.nextEvent();
-              if(startElement.getName().getLocalPart().equals("id")) {
-                xmlEvent = xmlEventReader.nextEvent();
-                curRevision.setRevisionId(Long.parseLong(xmlEvent.asCharacters().getData()));
-              }
-              else if(startElement.getName().getLocalPart().equals("parentid")) {
-                    xmlEvent = xmlEventReader.nextEvent();
-                    curRevision.setRevisionParentId(Long.parseLong(xmlEvent.asCharacters().getData()));
-              }
-              else if(startElement.getName().getLocalPart().equals("timestamp")) {
-                    xmlEvent = xmlEventReader.nextEvent();
-                    curRevision.setTimestamp(xmlEvent.asCharacters().getData());
-              }
-              // PERCORRE O CONTRIBUTOR
-              else if(startElement.getName().getLocalPart().equals("contributor")) {
-                    xmlEvent = xmlEventReader.nextEvent();
-                    while(xmlEventReader.hasNext()) {
-                      xmlEvent = xmlEventReader.nextEvent();
-                      if(startElement.getName().getLocalPart().equals("username")) {
-                        xmlEvent = xmlEventReader.nextEvent();
-                        curUser.setUsername(xmlEvent.asCharacters().getData());
-                      }
-                      else if(startElement.getName().getLocalPart().equals("id")) {
-                        xmlEvent = xmlEventReader.nextEvent();
-                        curUser.setUserId(Long.parseLong(xmlEvent.asCharacters().getData()));
-                      }
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(fileName));
+
+            while(eventReader.hasNext()){
+              XMLEvent event = eventReader.nextEvent();
+              switch(event.getEventType()){
+                  case XMLStreamConstants.START_ELEMENT:
+                    StartElement startElement = event.asStartElement();
+                    String qName = startElement.getName().getLocalPart();
+                    if (qName.equalsIgnoreCase("page")) {
+                      curArticle = new Article();
+                      curUser = new User();
+                      System.out.println("Start Element : page");
+                    } else if (qName.equalsIgnoreCase("title")) {
+                        hasPageTitle = true;
+                    } else if (qName.equalsIgnoreCase("id") && onContributor) {
+                        hasContributorId = true;
+                    } else if (qName.equalsIgnoreCase("revision")) {
+                        onRevision = true;
+                        curRevision = new Revision();
+                        System.out.println("Start Element : revision");
+                    } else if (qName.equalsIgnoreCase("id") && onRevision) {
+                        hasRevisionId = true;
+                    } else if (qName.equalsIgnoreCase("parentid")) {
+                        hasRevisionParentId = true;
+                    } else if (qName.equalsIgnoreCase("timestamp")) {
+                        hasRevisionTimestamp = true;
+                    } else if (qName.equalsIgnoreCase("contributor")) {
+                        onContributor = true;
+                        System.out.println("Start Element : contributor");
+                    } else if (qName.equalsIgnoreCase("username")) {
+                        hasContributorUsername = true;
+                    } else if (qName.equalsIgnoreCase("id")) {
+                        hasPageId = true;
                     }
+                    break;
+                  case XMLStreamConstants.CHARACTERS:
+                    Characters characters = event.asCharacters();
+                    if(hasPageTitle){
+                        curArticle.setArticleTitle(characters.getData());
+                        System.out.println("Title: " + curArticle.getArticleTitle() + "\n");
+                        hasPageTitle = false;
+                    }
+                    if(hasPageId){
+                        curArticle.setArticleId(Long.parseLong(characters.getData()));
+                        System.out.println("Article id: " + curArticle.getArticleId() + "\n");
+                        hasPageId = false;
+                    }
+                    if(hasRevisionId){
+                        curRevision.setRevisionId(Long.parseLong(characters.getData()));
+                        System.out.println("Revision id: " + curRevision.getRevisionId() + "\n");
+                        hasRevisionId = false;
+                        onRevision = false;
+                    }
+                    if(hasRevisionParentId){
+                        curRevision.setRevisionParentId(Long.parseLong(characters.getData()));
+                        System.out.println("Revision Parent id: " + curRevision.getRevisionParentId() + "\n");
+                        hasRevisionParentId = false;
+                    }
+                    if(hasRevisionTimestamp){
+                        curRevision.setTimestamp(characters.getData());
+                        System.out.println("Revision timestamp: " + curRevision.getTimestamp() + "\n");
+                        hasRevisionTimestamp = false;
+                    }
+                    if(hasContributorUsername){
+                        curUser.setUsername(characters.getData());
+                        System.out.println("User name: " + curUser.getUsername() + "\n");
+                        hasContributorUsername = false;
+                    }
+                    if(hasContributorId){
+                        curUser.setUserId(Long.parseLong(characters.getData()));
+                        System.out.println("User id: " + curUser.getUserId() + "\n");
+                        hasContributorId = false;
+                        onContributor = false;
+                    }
+                    break;
+                  case  XMLStreamConstants.END_ELEMENT:
+                    EndElement endElement = event.asEndElement();
+                    if(endElement.getName().getLocalPart().equalsIgnoreCase("page")){
+                        articles.put(curArticle.getArticleId(), curArticle);
+                        users.put(curUser.getUserId(), curUser);
+                        System.out.println("End Element : page");
+                        System.out.println();
+                    }
+                    break;
               }
             }
-          }
-        }
-
-        //if Employee end element is reached, add employee object to list
-        if(xmlEvent.isEndElement()){
-          EndElement endElement = xmlEvent.asEndElement();
-          if(endElement.getName().getLocalPart().equals("page")){
-            articles.put(curArticle.getArticleId(), curArticle);
-            users.put(curUser.getUserId(), curUser);
-          }
-        }
-      }
-
-    } catch (FileNotFoundException | XMLStreamException e) {
-      e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
     }
   }
 
